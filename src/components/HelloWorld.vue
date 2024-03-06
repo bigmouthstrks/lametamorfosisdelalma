b1c39e164d22496890a4cd0e3930d5fb65f17d57
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2024-03-06 10:57:09 -0300

    AWS

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="container">
-    <img src="./../assets/background.jpg" alt="Fondo" class="background">
+    <img src="https://i.pinimg.com/originals/36/07/c6/3607c693b6c03fa234b7c0700f965526.jpg" alt="Fondo" class="background">
     <div class="centered-image-container">
-      <img src="./../assets/avatar.jpg" alt="Imagen centrada" class="centered-image">
+      <img src="https://lametamorfosisdelalma.s3.sa-east-1.amazonaws.com/Captura%20de%20pantalla%202024-03-06%20a%20la%28s%29%2010.52.10.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEK7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXNhLWVhc3QtMSJHMEUCIQCIDr7E%2Fw6EqNpIiEEniNZIYcYE8wmezQPMaD1zB3cFHwIgHz1zQlzTK3oMgqlS9Z65u446f8KfTezgLekHsmgLCOIq8QIIp%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgw5MTY2NzE5OTYyNDciDKKYya%2BAxo6V7ohs7yrFAoxD2t0JQj5o9i73crJIpo71EwSjLo%2BgjBULfhrSc3wjJ7IIuxmSzffCvAUiNyWpXQVK3QsiWaF95r6Ot7zT3S%2FDoAXlJGAm2IxpBUe9jfnxgSE9v6bgPHgXY%2FZOaK6DaK96RGnoyyQrDC8TT%2BNr3iyWngtUwaym0qQ0Tnl5%2F2SQNOLWLOlwmKZZIjDDaK2wXLLWJrOfKFARUeQtW1DHqL8ECK88waDYg%2FuzmlXeuPJ%2Bb3QUkOdh%2B%2FcdtnxXm52FWwBW%2F0DWMszKVG2wwfnIV%2Br3cDY4ZjO0JEe7mstHfUvam6jHu%2B2aJQpvK8TnREP%2Byuz3IwgSXO4EDTMsfVjOPTmIkt8krOOcIRlDm9WmtUPHVicWtSRLh8i%2BsiYlpWl8F6ubb%2BzL3Adc3q%2Bs6%2FrRhNjpeV4cMbP%2Fyg5vNTOec03se4WCzocw4uehrwY6swK%2FIhLjN9Nyl4bZ6uuf77ANeOUFir0VgByWm%2FGKukWd6R7Qtgwml7YY8z9vhg47BpP0Pr5TSFWRgL6n49iKgHPrbkCos0zmxOv0XHdG74UDA%2BBTNKrH33YAzhRAOZUvEYrdEhHHn6vNyP6bRLVuFXVHPo%2FE1wz12TKF9Lf6QVDAd5luqZmuy%2BK1f1gyyub7WaDd%2BEn3kwTL77rNRqKwLht0nUlf7IlG5%2B5xZ5%2BpznVmwahwPS6OYTdsnK%2FMs8MWBu1LoLUMcAIoYt1xf81Pc7oH5MqEXFvsk4qT16RLZraVJIfBhnYi6yABqqX5Q0BZMzbckB5mlVIhKFJ4f1NNCxriuaLctTLiplvrOehEP8fjFpP%2FKLjlkM7sKLpinayBb2rFuXQklWBsjneWOCahxuLnTbzy&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240306T135302Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIA5K3PIUFLZK4TMCRM%2F20240306%2Fsa-east-1%2Fs3%2Faws4_request&X-Amz-Signature=f45dd702e21573ec5a8019a9afef4c6e765be9c0c111f84398437e3b471a6727" alt="Imagen centrada" class="centered-image">
     </div>
-    <h2 style="padding: 8px; color: white !important;"><strong>La Metamorfosis del Alma</strong></h2>
+    <h2 style="padding: 12px; color: white !important;"><strong>La Metamorfosis del Alma</strong></h2>
+    <div>
+      <ul>
+        <li>Acompañamiento terapéutico</li>
+        <li>Lectura de tarot</li>
+        <li>Canalizaciones</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -34,8 +41,8 @@
 }
 
 .centered-image {
-  width: 100%;
-  height: 100%;
+  width: 250px;
+  height: 250%;
   opacity: 1; /* Opacidad del 40% */
   border-radius: 50%; /* Borde circular */
 }
