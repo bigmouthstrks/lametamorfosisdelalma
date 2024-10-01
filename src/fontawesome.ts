9c753503becf6f88bbfd264a0f192ddd7524f79f
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2024-10-01 00:09:30 -0300

    feat: update font awesome dependencies

--- src/fontawesome.ts
+++ src/fontawesome.ts
@@ -4,6 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons' // Import all solid icons
 
 // Add the entire solid icons set to the library
-library.add(fas)
+library.add(...Object.values(fas))
 
 export default FontAwesomeIcon
